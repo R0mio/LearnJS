5d24e7e4dea38b1722c96bac5c99e1a05d8e7792
R0mio <roma_kudryashov@mail.ru>
2020-12-04 08:25:04 +0300

    Решение 3.1Slider

---
+++ 3InterfaceIvents/3.1Slider/1.js
@@ -0,0 +1,25 @@
+let thumb = document.querySelector('.thumb');
+let slider = document.getElementById('slider');
+
+thumb.onmousedown = function(event) {
+  let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  function onMouseMove(event) {
+    let slideMove = event.clientX - shiftX - slider.getBoundingClientRect().left;
+    if (0 < slideMove && slideMove < slider.offsetWidth - thumb.offsetWidth) {
+      thumb.style.left = slideMove + 'px'; 
+    }
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+};
+
+thumb.ondragstart = function() {
+  return false;
+};
